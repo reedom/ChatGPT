bf0dffb65c29880453e1b749e96f45d647859bb6
HANAI tohru <tohru@reedom.com>
2023-03-19 14:54:42 +0900

    update: change tts introduction

--- src/view/settings/TextToSpeech/GoogleTtsVoices.tsx
+++ src/view/settings/TextToSpeech/GoogleTtsVoices.tsx
@@ -32,18 +32,3 @@ export default function GoogleTtsVoices() {
     </Select>
   );
 }
-
-export function LoadCredentialButton() {
-  const { enabledGoogleTts, loadCredential } = useGoogleTtsState();
-
-  return (
-    <Button
-      style={{ marginBottom: 10 }}
-      className="chat-add-btn"
-      type="primary"
-      onClick={loadCredential}
-    >
-      {enabledGoogleTts ? 'Set Credential' : 'Load Credential JSON'}
-    </Button>
-  );
-}
--- src/view/settings/TextToSpeech/index.tsx
+++ src/view/settings/TextToSpeech/index.tsx
@@ -1,7 +1,8 @@
 import { Alert, Radio, RadioChangeEvent, Typography } from 'antd';
 import useFormInstance from 'antd/es/form/hooks/useFormInstance';
+import { open } from '@tauri-apps/api/shell';
 import BrowserBundledVoices from './BrowserBundledVoices';
-import GoogleTtsVoices, { LoadCredentialButton } from '@view/settings/TextToSpeech/GoogleTtsVoices';
+import GoogleTtsVoices from '@view/settings/TextToSpeech/GoogleTtsVoices';
 import { GoogleTtsStateProvider, useGoogleTtsState } from './useGoogleTtsState';
 import {
   SpeechService,
@@ -21,13 +22,18 @@ export default function TextToSpeech() {
 
 function Component() {
   const form = useFormInstance();
-  const { enabledGoogleTts, hasNewCredential } = useGoogleTtsState();
+  const { enabledGoogleTts, hasNewCredential, loadCredential } = useGoogleTtsState();
   const { speechService, setSpeechService } = useTtsState();
 
   const onChange = (e: RadioChangeEvent) => {
     setSpeechService(e.target.value);
     form.setFieldValue('speech_service', e.target.value);
   };
+
+  const openTtsTutorial = () =>
+    open(
+      'https://console.cloud.google.com/welcome?walkthrough_id=text-to-speech--text-to-speech-nodejs',
+    );
 
   return (
     <>
@@ -39,24 +45,22 @@ function Component() {
         <Radio value={SpeechService.google} disabled={!enabledGoogleTts}>
           Google Cloud Text-to-Speech
         </Radio>
-        <div style={{ padding: '12px 0 0 12px' }}>
+        <div style={{ padding: '12px 0 0 24px' }}>
           <GoogleTtsVoices />
         </div>
       </Radio.Group>
       {!enabledGoogleTts && (
-        <div style={{ padding: '12px 0 0 4px' }}>
+        <div style={{ padding: '12px 0 0 14px' }}>
           <Typography.Text italic>
-            To use Google Cloud Text-to-Speech service, fetch a GCP credential JSON and set it
-            below.
+            To use Google Cloud Text-to-Speech service,{' '}
+            <a onClick={openTtsTutorial}>generate a GCP credential JSON for it</a> and{' '}
+            <a onClick={loadCredential}>set the JSON by clicking here</a>.
             <br />
             (the credential file will be stored locally. Only used for the Text-to-Speech service
             and never been uploaded anywhere.)
           </Typography.Text>
         </div>
       )}
-      <div style={{ padding: '12px 0 4px' }}>
-        <LoadCredentialButton />
-      </div>
       {hasNewCredential && (
         <Alert
           style={{ marginBottom: '20px' }}
@@ -65,6 +69,7 @@ function Component() {
           showIcon
         />
       )}
+      <div style={{ padding: '12px' }} />
     </>
   );
 }
